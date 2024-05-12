b7006f0c22824befcf09d1dad4cf259d034edd25
Dmitrii Osipov <dimaosipov907@gmail.com>
2024-05-12 12:32:36 +0300

    Добавьте файлы проекта.

---
+++ Wordle/Headers/Data.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+using namespace sf;
+
+namespace texture
+{
+    extern std::vector<Texture> letters;
+
+    extern Texture A;
+    extern Texture B;
+    extern Texture C;
+    extern Texture D;
+    extern Texture E;
+    extern Texture F;
+    extern Texture G;
+    extern Texture H;
+    extern Texture I;
+    extern Texture J;
+    extern Texture K;
+    extern Texture L;
+    extern Texture M;
+    extern Texture N;
+    extern Texture O;
+    extern Texture P;
+    extern Texture Q;
+    extern Texture R;
+    extern Texture S;
+    extern Texture T;
+    extern Texture U;
+    extern Texture V;
+    extern Texture W;
+    extern Texture X;
+    extern Texture Y;
+    extern Texture Z;
+
+    void loadTextures();
+}
---
+++ Wordle/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Headers/Data.hpp"
+
+using namespace sf;
+
+int main() 
+{
+	RenderWindow window(VideoMode(1920, 1080), "Wordle", Style::Fullscreen);
+
+	texture::loadTextures();
+
+
+	while (window.isOpen()) 
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed) 
+			{
+				window.close();
+			}
+		}
+
+		window.clear(Color::Black);
+
+		window.display();
+	}
+
+	return 0;
+}
